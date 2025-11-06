a45027141870a1e099c83be41f1ca3a1d64b593f
Anna <cameaann@gmail.com>
2025-11-06 13:17:49 +0200

    editing About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,15 +11,21 @@ const About = () => {
       </div>
       <h1>Who am I</h1>
       <p>
-        I'm a frontend / full-stack developer. I studied Information Technologies at
-        Northern Arkhangelsk Federal University, and here in Finland I completed
-        the Java and React Coder course at Helsinki Business College. I have
-        about a year of professional experience, including internships at Unfair
-        Advantage and Kassavirtanen.
+        I’m a Frontend / Full-Stack Developer. I moved to Finland three years
+        ago and recently completed my studies in Information Technology at
+        Helsinki Business College, specializing in Fullstack Development with a
+        focus on Java and React.
       </p>
       <p>
-        I'm a mother of two teenagers, and a member
-        of Helsinki Urban Sketchers. I like to play board games and travel.
+        Before that, I studied Information Technologies at Northern (Arctic) Federal
+        University in Arkhangelsk. I have about a year of professional
+        experience, including internships at Unfair Advantage and Kassavirtanen,
+        where I contributed to real-world projects and gained hands-on
+        experience in modern web development.
+      </p>
+      <p>
+        I'm a mother of two teenagers, and a member of Helsinki Urban Sketchers.
+        In my free time, I enjoy playing board games and traveling.
       </p>
     </section>
   );
